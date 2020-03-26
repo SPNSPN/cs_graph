5441a1db290d062933a9d02a003b5da95efa71dd
SPNSPN <kusamoti@gmail.com>
2020-03-27 02:00:18 +0900

    mod

--- GraphForm.cs
+++ GraphForm.cs
@@ -3,25 +3,78 @@ using System.ComponentModel;
 using System.Drawing;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
+using System.IO;
+using System.IO.Ports;
+using System.Text;
 
 namespace GraphForm
 {
 class GraphForm : Form
 {
 	private Chart chart;
+	private Button log_btn;
+	private Button open_btn;
+	private TextBox port_text;
+	private TextBox log_text;
+
+	private SerialPort serial;
+	private double[] data;
+
+	private void open_Click (object sender, EventArgs e)
+	{
+		this.serial.PortName = this.port_text.Text;
+		this.serial.Open();
+	}
+
+	private void log_Click (object sender, EventArgs e)
+	{
+		File.WriteAllText(".\\log.csv", String.Join(", ", this.data));
+	}
 
 	public GraphForm ()
 	{
+		this.Size = new Size(800, 600);
+
+		this.log_btn = new Button();
+		this.log_btn.Text = "LOG";
+		this.log_btn.Click += new EventHandler(this.log_Click);
+		this.log_btn.Dock = DockStyle.Bottom;
+		this.log_btn.Size = new Size(0, 25);
+
+		this.open_btn = new Button();
+		this.open_btn.Text = "OPEN";
+		this.open_btn.Click += new EventHandler(this.open_Click);
+		this.open_btn.Dock = DockStyle.Bottom;
+		this.open_btn.Size = new Size(0, 25);
+
 		this.chart = new Chart();
 		this.chart.ChartAreas.Add(new ChartArea("area"));
 		this.chart.Series.Add("series");
 		this.chart.Series["series"].ChartType = SeriesChartType.Line;
 
-		double[] ys = new double[] {0.00, 0.18, 0.15, -0.05, 0.02};
-		for (int i = 0; i < ys.Length; ++i)
+		this.data = new double[] {0.00, 0.18, 0.15, -0.05, 0.02};
+		for (int i = 0; i < this.data.Length; ++i)
 		{
-			this.chart.Series["series"].Points.AddY(ys[i]);
+			this.chart.Series["series"].Points.AddY(this.data[i]);
 		}
+
+		this.port_text = new TextBox();
+		this.port_text.Dock = DockStyle.Bottom;
+
+		this.log_text = new TextBox();
+		this.log_text.Dock = DockStyle.Bottom;
+
+		this.serial = new SerialPort();
+		this.serial.BaudRate = 115200;
+		this.serial.Parity = Parity.None;
+		this.serial.DataBits = 8;
+		this.serial.StopBits = StopBits.One;
+		this.serial.Handshake = Handshake.None;
+
+		this.Controls.Add(this.port_text);
+		this.Controls.Add(this.open_btn);
+		this.Controls.Add(this.log_text);
+		this.Controls.Add(this.log_btn);
 		this.Controls.Add(this.chart);
 	}
 }
--- Makefile
+++ Makefile
@@ -4,6 +4,8 @@ LIB := 'C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319,C:\\Windows\Microsof
 Main.exe: Main.cs GraphForm.cs
 	$(CSC) /lib:$(LIB) /reference:C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\System.Windows.Forms.DataVisualization.dll Main.cs GraphForm.cs
 
-.PHONY: clean
+.PHONY: run clean
+run: Main.exe
+	Main.exe
 clean:
 	rm Main.exe
